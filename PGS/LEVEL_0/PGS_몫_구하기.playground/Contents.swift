fa959d88930cd980a112d2e3010e7d489502db95
derrickkim0109 <derrickkim0109@naver.com>
2022-10-23 14:03:14 +0900

    feat: [PGS] 몫구하기 / Level0 / 10초
    https://school.programmers.co.kr/learn/courses/30/lessons/120805

---
+++ PGS/LEVEL_0/PGS_몫_구하기.playground/Contents.swift
@@ -0,0 +1,15 @@
+//
+//  PGS_몫_구하기.playground <- 문제명
+//  algorithm-study-in-swift <- 프로젝트명
+//
+//  Created by derrick on 2022/09/21.
+//  https://school.programmers.co.kr/learn/courses/30/lessons/120805 <- 문제 주소
+
+import Foundation
+
+func solution_PGS_몫_구하기(_ num1:Int, _ num2:Int) -> Int {
+    let result = num1 / num2
+    return result
+}
+
+print(solution(10, 5))
