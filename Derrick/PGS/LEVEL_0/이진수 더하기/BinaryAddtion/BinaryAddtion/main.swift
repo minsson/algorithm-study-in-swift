77858afeb5bc0404d8af2c2f61bbc2dccf10a3b2
derrickkim0109 <derrickkim0109@naver.com>
2022-10-30 16:07:10 +0900

    feat: 이진수 더하기 (Binary Addition)

---
+++ Derrick/PGS/LEVEL_0/이진수 더하기/BinaryAddtion/BinaryAddtion/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  BinaryAddtion
+//
+//  Created by Derrick kim on 2022/10/30.
+//
+
+import Foundation
+
+func solution(_ bin1:String, _ bin2:String) -> String {
+
+    var answer = ""
+    if let number1 = Int(bin1, radix: 2), let number2 = Int(bin2, radix: 2) {
+        answer = String(number1 + number2, radix: 2)
+    }
+
+    return answer
+}
+
