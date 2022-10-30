538072a627f2d5145f47a08c8695e25879ff4010
MinsuJeon <chso294@naver.com>
2022-10-30 18:05:34 +0900

    [BOJ] A+B / 브론즈5 / 1분
    https://www.acmicpc.net/problem/1000

---
+++ SooKoong/AlogithmStudy/AlogithmStudy/BOJ/Bronze/BOJ_HelloWorld.swift
@@ -0,0 +1,12 @@
+//
+//  BOJ_HelloWorld.swift
+//  AlogrithmsStudy-Sookoong
+//
+//  Created by 수꿍 on 2022/10/30.
+//  https://www.acmicpc.net/problem/2557
+
+import Foundation
+
+func answer_BOJ_HelloWorld_2557() {
+    print("Hello World!")
+}
