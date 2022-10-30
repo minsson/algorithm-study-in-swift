4d89fac959ef41d4f1911116ab148091497e5ea5
bard <schy2503@naver.com>
2022-10-30 17:42:30 +0900

    [티어전][바드🏅, 수꿍][BOJ] 사칙연산/브론즈/3분
    https://www.acmicpc.net/problem/10998

---
+++ Bard/AlgorithmStudy/AlgorithmStudy/BOJ/Bronze/BOJ_A곱하기B_10998.swift
@@ -0,0 +1,8 @@
+//
+//  BOJ_A곱하기B_10998.swift
+//  AlgorithmStudy
+//
+//  Created by bard on 2022/10/30.
+//
+
+import Foundation
--- Bard/AlgorithmStudy/AlgorithmStudy/main.swift
+++ Bard/AlgorithmStudy/AlgorithmStudy/main.swift
@@ -11,5 +11,7 @@ import Foundation
 
 /*
 answer_BOJ_A플러스B_1000()
+ answer_BOJ_A마이너스B_1001()
 */
-answer_BOJ_A마이너스B_1001()
+
+answer_BOJ_사칙연산_10869()
