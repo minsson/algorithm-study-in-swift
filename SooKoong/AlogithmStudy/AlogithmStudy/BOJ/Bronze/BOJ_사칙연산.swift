9f9f37161c116d1f69e9f6d0dfb0885ed648914d
MinsuJeon <chso294@naver.com>
2022-10-30 18:06:24 +0900

    [티어전][바드🏅, 수꿍][BOJ] 사칙연산/브론즈/10분
    https://www.acmicpc.net/problem/10869

---
+++ SooKoong/AlogithmStudy/AlogithmStudy/BOJ/Bronze/BOJ_사칙연산.swift
@@ -0,0 +1,23 @@
+//
+//  BOJ_사칙연산.swift
+//  AlogrithmsStudy-Sookoong
+//
+//  Created by 수꿍 on 2022/10/30.
+//  https://www.acmicpc.net/problem/10869
+
+import Foundation
+
+func answer_BOJ_사칙연산_10869() {
+    let input = readLine()!
+    let string = input.components(separatedBy: " ")
+    let a = Int(string.first ?? "0")!
+    let b = Int(string.last ?? "0")!
+
+    if a <= 10000, b <= 10000 {
+        print(a + b)
+        print(a - b)
+        print(a * b)
+        print(a / b)
+        print(a % b)
+    }
+}
