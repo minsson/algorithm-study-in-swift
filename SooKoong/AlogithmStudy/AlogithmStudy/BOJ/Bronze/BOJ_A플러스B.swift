26873cc8ba9966c07dc570f282d30daa7f5597a9
MinsuJeon <chso294@naver.com>
2022-10-30 18:05:12 +0900

    [BOJ] HelloWorld / 브론즈5 / 1분
    https://www.acmicpc.net/problem/2557

---
+++ SooKoong/AlogithmStudy/AlogithmStudy/BOJ/Bronze/BOJ_A플러스B.swift
@@ -0,0 +1,18 @@
+//
+//  BOJ_A플러스B.swift
+//  AlogrithmsStudy-Sookoong
+//
+//  Created by 수꿍 on 2022/10/30.
+//  https://www.acmicpc.net/problem/1000
+
+import Foundation
+
+func answer_BOJ_A플러스B_1000() {
+    let input = readLine()!.components(separatedBy: " ")
+    let a = Int(input.first!)!
+    let b = Int(input.last!)!
+
+    if a > 0, a < 10, b > 0, b < 10 {
+        print(a + b)
+    }
+}
