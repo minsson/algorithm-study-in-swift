e487a2134578cf87381a9096af6301bee56e163e
unchain123 <unchain123@kakao.com>
2022-11-02 15:23:24 +0900

    [BOJ] / 시험성적 / 사분면고르기

--- unchain/main/main/main.swift
+++ unchain/main/main/main.swift
@@ -9,5 +9,9 @@ import Foundation
 
 //MARK: - 백준 Bronze
 
-answer_BOJ_AxB_10998()
-answer_BOJ_10926()
+//answer_BOJ_AxB_10998()
+//answer_BOJ_10926()
+
+//MARK: - 백준 Bronze 2주차
+answer_BOJ_사분면고르기_14681()
+answer_BOJ_시험성적_9498()
