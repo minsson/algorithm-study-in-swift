5403f8e800bbbb49b16cfd085ccf2943294ce4cd
Judy <joo00ee@naver.com>
2022-11-02 19:04:38 +0900

    [PGS] 콜라 문제 / Lv.1 / 8분 5초
    https://school.programmers.co.kr/learn/courses/30/lessons/132267

--- Judy/AlogithmStudy/AlogithmStudy/main.swift
+++ Judy/AlogithmStudy/AlogithmStudy/main.swift
@@ -10,4 +10,5 @@ import Foundation
 //MARK: - 프로그래머스 Level 1
 //print(answer_PGS_부족한_금액_계산(1000, 8000, 4))
 //print(answer_PGS_점의_위치_구하기([2,4]))
-print(dotProduct([1, 2, 3], [4, 5, 6]))
+//print(dotProduct([1, 2, 3], [4, 5, 6]))
+print(cokeProblem(2, 1, 20))
