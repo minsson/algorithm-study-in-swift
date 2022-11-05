fc4dd87b0ab016ac0dd16ddb4f9a9c78b3107b66
bard <schy2503@naver.com>
2022-11-05 18:47:01 +0900

    [BOJ] A나누기B / 브론즈5 / 1분
    https://www.acmicpc.net/problem/1008

--- Bard/AlgorithmStudy/AlgorithmStudy/main.swift
+++ Bard/AlgorithmStudy/AlgorithmStudy/main.swift
@@ -14,6 +14,7 @@ import Foundation
  answer_BOJ_A마이너스B_1001()
  answer_BOJ_사칙연산_10869()
  answer_BOJ_A곱하기B_10998()
+ answer_BOJ_물음표물음표느낌표_10926()
 */
 
-answer_BOJ_물음표물음표느낌표_10926()
+BOJ_A나누기B_1008()
