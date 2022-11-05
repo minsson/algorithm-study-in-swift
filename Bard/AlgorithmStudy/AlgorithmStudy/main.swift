01c769f4e3e96fada789f183d9589a4dabfc25cd
bard <schy2503@naver.com>
2022-11-05 18:28:46 +0900

    [BOJ] A*B / 브론즈5 / 1분
    https://www.acmicpc.net/problem/10998

--- Bard/AlgorithmStudy/AlgorithmStudy/main.swift
+++ Bard/AlgorithmStudy/AlgorithmStudy/main.swift
@@ -10,8 +10,9 @@ import Foundation
 // MARK: - 백준 Bronze 5
 
 /*
-answer_BOJ_A플러스B_1000()
+ answer_BOJ_A플러스B_1000()
  answer_BOJ_A마이너스B_1001()
+ answer_BOJ_사칙연산_10869()
 */
 
-answer_BOJ_사칙연산_10869()
+answer_BOJ_A곱하기B_10998()
