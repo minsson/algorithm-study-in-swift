1673ee2444842d6c3e69772cd2247c6ebef5afe1
Judy <joo00ee@naver.com>
2022-11-09 14:43:26 +0900

    [PGS] 치킨 쿠폰 / Lv.0 / 4분
    https://school.programmers.co.kr/learn/courses/30/lessons/120884

--- Judy/AlogithmStudy/AlogithmStudy/main.swift
+++ Judy/AlogithmStudy/AlogithmStudy/main.swift
@@ -11,4 +11,5 @@ import Foundation
 //print(answer_PGS_부족한_금액_계산(1000, 8000, 4))
 //print(answer_PGS_점의_위치_구하기([2,4]))
 //print(dotProduct([1, 2, 3], [4, 5, 6]))
-print(cokeProblem(2, 1, 20))
+//print(cokeProblem(2, 1, 20))
+print(chickenCoupon(100))
