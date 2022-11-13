93eb1b396f30b31b99afa159a8811196089d7580
apwierk2451 <apwierk2451@gmail.com>
2022-11-13 15:54:31 +0900

    [BOJ] 시험 성적 / Bronze5 / 5분

--- Bonf/AlgorithmStudy/AlgorithmStudy/main.swift
+++ Bonf/AlgorithmStudy/AlgorithmStudy/main.swift
@@ -5,11 +5,11 @@
 //  Created by 이은찬 on 2022/11/02.
 //
 
-import Foundation
-
 //answer_BOJ_A플러스B_1000()
 //answer_BOJ_모음의개수_1264()
 //let abc = ["ayaye", "uuuma", "ye", "yemawoo", "ayaa"]
 //print(solution(abc))
 //answer_BOJ_selfNumber()
-answer_BOJ_A마이너스B_1001()
+//answer_BOJ_A마이너스B_1001()
+//answer_BOJ_두수비교하기_1330()
+answer_BOJ_시험성적_9498()
