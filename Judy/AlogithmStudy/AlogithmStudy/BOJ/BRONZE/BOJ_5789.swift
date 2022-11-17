5d0f0fe07872066e01006fda948edec5965d0d4d
Judy <joo00ee@naver.com>
2022-11-17 16:30:42 +0900

    [BOJ] 한다 안한다 / 브론즈3 / 15분
    https://www.acmicpc.net/problem/5789

---
+++ Judy/AlogithmStudy/AlogithmStudy/BOJ/BRONZE/BOJ_5789.swift
@@ -0,0 +1,33 @@
+//
+//  DoItDoItNot.swift
+//  AlogithmStudy
+//
+//  Created by Judy on 2022/11/17.
+//  https://www.acmicpc.net/problem/5789
+
+
+func anwser_BOJ_한다안한다_5789() {
+    let readNumber = readLine()
+    let count = Int(readNumber ?? "0") ?? 0
+    var array: [[String]] = []
+    var result = false
+    
+    for _ in 1...count {
+        let randomArray = readLine() ?? "0"
+        array.append(randomArray.map { String($0) })
+    }
+    
+    for number in array {
+        var list = number
+        
+        while !list.isEmpty {
+            if list.removeFirst() == list.removeLast() {
+                result = true
+            } else {
+                result = false
+            }
+        }
+        
+        print(result ? "Do-it" : "Do-it-Not")
+    }
+}
--- Judy/AlogithmStudy/AlogithmStudy/main.swift
+++ Judy/AlogithmStudy/AlogithmStudy/main.swift
@@ -7,10 +7,16 @@
 
 import Foundation
 
+//MARK: - 프로그래머스 Level 0
+//print(answer_PGS_점의_위치_구하기([2,4]))
+
 //MARK: - 프로그래머스 Level 1
 //print(answer_PGS_부족한_금액_계산(1000, 8000, 4))
-//print(answer_PGS_점의_위치_구하기([2,4]))
+
 //print(dotProduct([1, 2, 3], [4, 5, 6]))
 //print(cokeProblem(2, 1, 20))
 //print(chickenCoupon(100))
-print(addMatrix([[1, 2], [3, 4]], [[5, 6], [7, 8]]))
+//print(addMatrix([[1, 2], [3, 4]], [[5, 6], [7, 8]]))
+
+//MARK: - 백준 BRONZE
+anwser_BOJ_한다안한다_5789()
