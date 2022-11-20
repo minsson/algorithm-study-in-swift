42deb6b924f4a36459e855845837575bf2e8f4cc
wongbingg <wb277@naver.com>
2022-11-20 13:50:42 +0900

    [PGS] 연습문제 / 레벨0 / 1시간
    - 입문용 문제풀이를 통해 프로그래머스 환경 적응

---
+++ wongbing/wongbing/PGS/LEVEL_0/PGS_두수의차.swift
@@ -0,0 +1,13 @@
+//
+//  PGS_두수의차.swift
+//  wongbing
+//
+//  Created by 이원빈 on 2022/11/19.
+// https://school.programmers.co.kr/learn/courses/30/lessons/120803
+
+func answer_PGS_두수의차() {
+    
+    func solution(_ num1:Int, _ num2:Int) -> Int {
+        return num1 - num2
+    }
+}
