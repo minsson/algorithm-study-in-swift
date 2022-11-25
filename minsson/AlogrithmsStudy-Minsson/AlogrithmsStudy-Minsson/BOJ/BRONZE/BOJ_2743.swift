b954fc02e58353eb9055e984a06b39bcabd63dac
minsson <mskaang@gmail.com>
2022-11-25 18:05:13 +0900

    [BOJ] 단어길이재기 / 브론즈5 / 2분

---
+++ minsson/AlogrithmsStudy-Minsson/AlogrithmsStudy-Minsson/BOJ/BRONZE/BOJ_2743.swift
@@ -0,0 +1,11 @@
+//
+//  BOJ_2743.swift
+//  AlogrithmsStudy-Minsson
+//
+//  Created by minsson on 2022/11/25.
+//  https://www.acmicpc.net/problem/2743
+
+func answer_BOJ_단어길이재기_2743() {
+    let input = readLine()!
+    print(input.count)
+}
