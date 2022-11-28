3b23e1f3dce4d521891a401796c0172f133e59cc
derrickkim0109 <derrickkim0109@naver.com>
2022-11-28 13:22:32 +0900

    refactor: [PGS] 실패율 / Level 1 / 1시간

--- Derrick/PGS/LEVEL_1/failurerate/failurerate/main.swift
+++ Derrick/PGS/LEVEL_1/failurerate/failurerate/main.swift
@@ -9,20 +9,24 @@ import Foundation
 
 func solution(_ N:Int, _ stages:[Int]) -> [Int] {
     var totalStageFailureRate: [Int: Double] = [:]
+    var stageTotal = Double(stages.count)
+    var currentStagePeople = Array(repeating: 0, count: N+2)
 
-    for level in 1...N {
-        let currentStagePeople = stages.filter{ $0 >= level }.count
-
-        let stageTotal = stages.filter{ $0 > level }.count
-
-        let failureRate = Double(currentStagePeople) / Double(stageTotal)
-
-        totalStageFailureRate.updateValue(failureRate, forKey: level)
+    for level in stages {
+        currentStagePeople[level] += 1
     }
 
-    let rate = totalStageFailureRate.sorted(by: <).sorted(by: { $0.value > $1.value })
+    for level in 1...N {
+        if currentStagePeople[level] == 0 {
+            totalStageFailureRate[level] = 0.0
+        } else {
+            stageTotal = stageTotal - Double(currentStagePeople[level])
+            totalStageFailureRate[level] = Double(currentStagePeople[level]) / Double(stageTotal)
+        }
 
-    return rate.map{ $0.key }
+    }
+
+    return totalStageFailureRate.sorted(by: <).sorted(by: { $0.value > $1.value }).map{ $0.key }
 }
 
 print(solution(4, [4,4,4,4,4]))
