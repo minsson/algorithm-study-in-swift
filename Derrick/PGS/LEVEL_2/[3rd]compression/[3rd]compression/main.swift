04f118c57977740819e0b2dff5039fb8e6aa05cc
derrickkim0109 <derrickkim0109@naver.com>
2022-11-29 19:13:21 +0900

    feat: [PGS] [3차] 압축 / Level 2 / 1시간 반

---
+++ Derrick/PGS/LEVEL_2/[3rd]compression/[3rd]compression/main.swift
@@ -0,0 +1,63 @@
+//
+//  main.swift
+//  [3rd]compression
+//
+//  Created by Derrick kim on 2022/11/29.
+//
+
+import Foundation
+
+func solution(_ msg:String) -> [Int] {
+    guard msg.count >= 1, msg.count <= 1000 else {
+        return []
+    }
+
+    var result: [Int] = []
+    let msgArray = msg.map{ String($0) }
+
+    var allWords: [String: Int] = createAlphabets()
+
+    var previousWord = ""
+    var count = 0
+
+    while count < msgArray.count {
+        let nextWord = msgArray[count]
+
+        let currentWord = previousWord + nextWord
+        let isEmptyWord = allWords.contains { $0.key == currentWord }
+
+
+        if !isEmptyWord {
+            result.append(allWords[previousWord]!)
+
+            allWords.updateValue(allWords.count + 1, forKey: currentWord)
+
+            previousWord = ""
+        }
+
+        previousWord += nextWord
+        count += 1
+    }
+
+    result.append(allWords[previousWord]!)
+
+    return result
+}
+
+func createAlphabets() -> [String: Int] {
+    var allAlphabet: [String: Int] = [:]
+    let startAlphbet = Unicode.Scalar("A").value
+    let endAlphbet = Unicode.Scalar("Z").value
+    var point = 1
+
+    for number in startAlphbet...endAlphbet {
+        if let alphabet = Unicode.Scalar(number) {
+            allAlphabet.updateValue(point, forKey: String(alphabet))
+            point += 1
+        }
+    }
+
+    return allAlphabet
+}
+
+print(solution("KAKAO"))
