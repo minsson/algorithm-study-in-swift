311aecd9a7c19fabad21237bd7aad2e967c12254
minsson <mskaang@gmail.com>
2022-12-11 21:53:54 +0900

    [PGS] 짝수와 홀수 / Level 1 / 2분
    https://school.programmers.co.kr/learn/courses/30/lessons/12937

---
+++ minsson/AlogrithmsStudy-Minsson/AlogrithmsStudy-Minsson/PGS/PGS_12937.swift
@@ -0,0 +1,16 @@
+//
+//  PGS_12937.swift
+//  AlogrithmsStudy-Minsson
+//
+//  Created by minsson on 2022/12/11.
+//  https://school.programmers.co.kr/learn/courses/30/lessons/12937
+
+import Foundation
+
+func answer_PGS_짝수와홀수_solution(_ num:Int) -> String {
+    if num % 2 == 0 {
+        return "Even"
+    } else {
+        return "Odd"
+    }
+}
