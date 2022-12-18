ab54a932a577c34221f29b2d4b7c4c866bbc3872
seohyeon2 <seo_hyeon418@naver.com>
2022-12-18 18:19:27 +0900

    [PGS] 암호 해독 / Level 0 / 10분
    - https://school.programmers.co.kr/learn/courses/30/lessons/120892

---
+++ Seohyeon2/Seohyeon2/PGS/LEVEL_0/PGS_120892.swift
@@ -0,0 +1,22 @@
+//
+//  PGS_120892.swift
+//  Seohyeon2
+//
+//  Created by seohyeon park on 2022/12/18.
+//
+// https://school.programmers.co.kr/learn/courses/30/lessons/120892
+
+import Foundation
+
+func answer_PGS_암호_해독_120891() {
+    func solution(_ cipher:String, _ code:Int) -> String {
+        let arr = cipher.map { String($0) }
+        var result = ""
+        for index in 0...cipher.count - 1 {
+            if (index + 1) % code == 0 {
+                result += arr[index]
+            }
+        }
+        return result
+    }
+}
--- Seohyeon2/Seohyeon2/main.swift
+++ Seohyeon2/Seohyeon2/main.swift
@@ -109,9 +109,10 @@ answer_PGS_A로_B_만들기_120886()
 answer_PGS_k의_개수_120887()
 answer_PGS_중복된_문자_제거_120888()
 answer_PGS_삼각형의_완성조건_1_120889()
+answer_PGS_가까운_수_120890()
 */
 
-answer_PGS_가까운_수_120890()
+answer_PGS_암호_해독_120891()
 
 //MARK: - 프로그래머스 Level 1
 
