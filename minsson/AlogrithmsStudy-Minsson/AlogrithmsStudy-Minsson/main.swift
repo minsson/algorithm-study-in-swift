a100d586fe4e99f98831d143e16e6df11c6723df
minsson <mskaang@gmail.com>
2022-12-18 23:44:52 +0900

    [PGS] 나머지구하기 / Level 0 / 3분
    https://school.programmers.co.kr/learn/courses/30/lessons/120810

--- minsson/AlogrithmsStudy-Minsson/AlogrithmsStudy-Minsson/main.swift
+++ minsson/AlogrithmsStudy-Minsson/AlogrithmsStudy-Minsson/main.swift
@@ -45,8 +45,9 @@ import Foundation
 
 //print(answer_PGS_가운데글자가져오기_solution("abcde"))
 //print(answer_PGS_평균구하기_solution([1,2,3,4]))
-print(answer_PGS_짝수와홀수_solution(3))
+//print(answer_PGS_짝수와홀수_solution(3))
 
-
+// MARK: - 프로그래머스 Level 0
+print(answer_PGS_나머지구하기_solution(3, 2))
 
 
