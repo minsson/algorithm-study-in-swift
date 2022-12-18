48783650e9835613204f571de1a3d7d16acc73c0
seohyeon2 <seo_hyeon418@naver.com>
2022-12-18 16:02:19 +0900

    [PGS] 두 수의 곱 / Level 0 / 1분
    - https://school.programmers.co.kr/learn/courses/30/lessons/120804

---
+++ Seohyeon2/Seohyeon2/PGS/LEVEL_0/PGS_120804.swift
@@ -0,0 +1,15 @@
+//
+//  PGS_120804.swift
+//  Seohyeon2
+//
+//  Created by seohyeon park on 2022/12/18.
+//
+// https://school.programmers.co.kr/learn/courses/30/lessons/120804
+
+import Foundation
+
+func answer_PGS_두_수의_곱_120804() {
+    func solution(_ num1:Int, _ num2:Int) -> Int {
+        return num1 * num2
+    }
+}
