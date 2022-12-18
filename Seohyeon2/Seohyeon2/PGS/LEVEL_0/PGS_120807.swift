b0c267e8415259a92a17adc032f1a61c9f5cac49
seohyeon2 <seo_hyeon418@naver.com>
2022-12-18 16:07:23 +0900

    [PGS] 숫자 비교하기 / Level 0 / 1분
    - https://school.programmers.co.kr/learn/courses/30/lessons/120807

---
+++ Seohyeon2/Seohyeon2/PGS/LEVEL_0/PGS_120807.swift
@@ -0,0 +1,15 @@
+//
+//  PGS_120807.swift
+//  Seohyeon2
+//
+//  Created by seohyeon park on 2022/12/18.
+//
+// https://school.programmers.co.kr/learn/courses/30/lessons/120807
+
+import Foundation
+
+func answer_PGS_숫자_비교하기_120807() {
+    func solution(_ num1:Int, _ num2:Int) -> Int {
+        return return num1 == num2 ? 1 : -1
+    }
+}
