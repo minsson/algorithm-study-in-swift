2bb18778d1a817dfb4009ee4806544bcf032710e
seohyeon2 <seo_hyeon418@naver.com>
2022-12-18 16:32:55 +0900

    [PGS] 나이 출력 / Level 0 / 1분
    - https://school.programmers.co.kr/learn/courses/30/lessons/120820

---
+++ Seohyeon2/Seohyeon2/PGS/LEVEL_0/PGS_120820.swift
@@ -0,0 +1,15 @@
+//
+//  PGS_120820.swift
+//  Seohyeon2
+//
+//  Created by seohyeon park on 2022/12/18.
+//
+// https://school.programmers.co.kr/learn/courses/30/lessons/120820
+
+import Foundation
+
+func answer_PGS_나이_출력_120820() {
+    func solution(_ age:Int) -> Int {
+        return 2022 - age + 1
+    }
+}
